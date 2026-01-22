7b20d88c4becb5e77c07ee72aa04cc75a2497047
Alexandre BAUDRY <alexandre.baudry@mousquetaires.com>
2026-01-22 14:49:34 +0100

    web socket message
    envoyer et lire un message en websocket

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,9 +1,79 @@
 import {io} from 'socket.io-client';
+import {MessageType} from "../types/message";
 
 const socket = io("ws://localhost:9000");
 socket.on('connection', () => {
     console.log("user connected");
-})
+});
+
+const messageContainer = document.getElementById('messages');
+const form = document.getElementById('type-message') as HTMLFormElement;
+
+const socketContainer = document.getElementById('socket');
+
+socket.on('connection', () => {
+    if(socketContainer) socketContainer.innerText = socket?.id ?? "";
+});
+
+
+form?.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const text = String(form?.elements['message'].value);
+    const message = {
+        id : crypto.randomUUID(),
+        text,
+        senderMessageId: socket.id,
+    } as MessageType;
+
+    socket.emit('send-message', message);
+    form.reset();
+});
+
+socket.on("new-message", (message: MessageType) => {
+    displayMessage(message);
+    if (message.senderMessageId === socket.id) return;
+    socket.emit("read-message", { messageId: message.id });
+});
+socket.on("message-updated", (message: MessageType) => {
+    displayMessage(message);
+});
+
+
+function displayMessage(message: MessageType) {
+    if (!messageContainer) return;
+
+
+    let li = document.getElementById(message.id) as HTMLLIElement | null;
+
+    if (!li) {
+        li = document.createElement("li");
+        li.id = message.id;
+        li.className = "message";
+
+        li.innerHTML = `
+            <div class="sender"></div>
+            <div class="content"></div>
+            <div class="readers"></div>
+        `;
+
+        messageContainer.appendChild(li);
+    }
+
+    li.querySelector(".sender")!.textContent =
+        `Envoyé par : ${message.senderMessageId}`;
+
+    li.querySelector(".content")!.textContent =
+        `Contenu : ${message.text}`;
+
+    // @ts-ignore
+    const readers = message?.readersMessageIds?.length > 0
+            ? message?.readersMessageIds?.join(", ")
+            : "Personne";
+
+    li.querySelector(".readers")!.textContent =
+        `Lu par : ${readers}`;
+}
 
 
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import {MessageType} from "../types/message";
 
 const PORT = 9000;
 const io = new Server(PORT, {
@@ -7,10 +8,45 @@ const io = new Server(PORT, {
     }
 });
 
+const messages = [] as MessageType[];
+
 io.on("connection", (socket) => {
-    console.log(`user connected: ${socket.id}`);
+
     socket.emit('connection');
+
+    socket.on("send-message", (message: MessageType) => {
+        const newMessage: MessageType = {
+            ...message,
+            readersMessageIds: []
+        };
+
+        messages.push(newMessage);
+        socket.broadcast.emit("new-message", message);
+    });
+
+    socket.on("read-message", ({ messageId,  }) => {
+        const message = messages.find(m => m.id === messageId);
+        if (!message) return;
+
+        if (socket.id === message.senderMessageId) return;
+
+        if (!message?.readersMessageIds?.includes(socket.id)) {
+            // @ts-ignore
+            message.readersMessageIds.push(socket.id);
+        }
+
+        socket.broadcast.emit("message-updated", message);
+        socket.emit("message-updated", message);
+    });
+
+
 });
+
+
+
+
+
+
 
 
 
